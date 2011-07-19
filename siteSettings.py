290da189670b5edfeed7a0c289de9fb82da5c926
Chris Corcoran <chris@thingsgood.com>
2011-07-18 17:00:22 -0700

    Added support for sending/tracking group invitations

--- siteSettings.py
+++ siteSettings.py
@@ -29,3 +29,25 @@ ROOT_URLCONF = 'geocamShare.urls'
 
 # DIGEST_* -- settings for django_digest HTTP digest authentication
 DIGEST_REALM = 'geocamshare.org'
+
+# Email Settings, override these to ensure that invites are sent succesfully
+EMAIL_HOST = ''
+EMAIL_PORT = ''
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+
+INVITE_SUBJECT = 'You have been invited to the %(groupname)s group'
+INVITE_FROM = EMAIL_HOST_USER
+INVITE_MESSAGE = """
+Hello,
+
+You have been invited to join the %(groupname)s on GeoCam.
+
+To join this group click the link bellow and follow the instructions:
+
+%(invitelink)s
+
+Thank You,
+The GeoCamTeam
+"""
